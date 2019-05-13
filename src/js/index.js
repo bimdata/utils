d503540cc060a0f3591718c8b80e0b1c59319b14
Gaelle <gaelle@bimdata.io>
2019-05-13 19:04:52 +0200

    add style for dark theme

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,30 @@ import '../scss/main.scss'
 
 require('../img/bimdata_logo.svg');
 
-document.getElementById('themeSwitch').addEventListener('change', function(event){
-  $(document.body).toggleClass('theme-dark');
-});
+const toggleSwitch = document.querySelector('#themeSwitch');
+const body = document.querySelector('body');
+let isDark = '';
 
+document.addEventListener('DOMContentLoaded', function(){
+  isDark = localStorage.getItem('state-dark');
+  if(isDark == 'Dark'){
+    toggleSwitch.setAttribute('checked', true);
+    body.classList.add('theme-dark');
+  }
+  else{
+    toggleSwitch.removeAttribute('checked');
+    body.classList.remove('theme-dark');
+  }
+
+  toggleSwitch.onchange = (e) => {
+    if(e.currentTarget.checked){
+      body.classList.add('theme-dark');
+      isDark = 'Dark';
+    }
+    else{
+      body.classList.remove('theme-dark');
+      isDark = '';
+    }
+    localStorage.setItem('state-dark', isDark);
+  }
+});
